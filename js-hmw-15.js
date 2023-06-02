132bb2a6b9ee722ea4767e84d18f6922fbb73331
z-chubinidze <120126922+z-chubinidze@users.noreply.github.com>
2023-06-02 19:23:54 +0400

    Add files via upload

---
+++ js-hmw-15.js
@@ -0,0 +1,12 @@
+
+function twitboxShow(){
+    var twit = document.getElementById("twitBox");
+
+    twit.style.display = "block";
+};
+
+function twitboxHide(){
+    var hidebtn = document.getElementById("twitBox"); 
+
+    hidebtn.style.display = "none";
+};
